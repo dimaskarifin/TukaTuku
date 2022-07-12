e55820bd61f22707a194049fa05efaf0f6d20cbe
Dimas Khulil Arifin <dimaskarifin@gmail.com>
2022-07-12 14:10:31 +0700

    Edit app.js, router, index.js di pages

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home, Splash} from '../pages';
+
+const Router = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;
